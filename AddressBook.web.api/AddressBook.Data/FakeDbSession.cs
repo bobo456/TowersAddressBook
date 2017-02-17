8d1744642f54699267465b14673c9a3e5cedb9c9
Rich Watts <bobo456@gmail.com>
2017-02-16 21:27:36 -0700

    Rich - updated some styles.

--- AddressBook.web.api/AddressBook.Data/FakeDbSession.cs
+++ AddressBook.web.api/AddressBook.Data/FakeDbSession.cs
@@ -14,13 +14,13 @@ namespace AddressBook.Data
 
 		public List<AddressBookEntry> InitializeAddressBookEntries()
 		{
-			var addressBookEntry1 = AddressBookEntry.Create("allen", "iverson", "123 All Star Way", null, "Philadelphia",
+			var addressBookEntry1 = AddressBookEntry.Create("allen", "iverson", "123 All Star Way", "", "Philadelphia",
 				"PA", "84001", "888-123-1234", "123-234-3456", "ai@nba.com");
-			var addressBookEntry2 = AddressBookEntry.Create("larry", "brown", "456 Coach Circle", null, "Philadelphia",
+			var addressBookEntry2 = AddressBookEntry.Create("larry", "brown", "456 Coach Circle", "", "Philadelphia",
 				"PA", "84002","866-234-4567", "231-456-7890", "lb@nba.com");
-			var addressBookEntry3 = AddressBookEntry.Create("jeremy", "clarkson", "789 Test Track", null, "Salt Lake City",
+			var addressBookEntry3 = AddressBookEntry.Create("jeremy", "clarkson", "789 Test Track", "", "Salt Lake City",
 				"UT", "84003", "877-345-6789", "789-456-1320", "jc@topGear.com");
-			var addressBookEntry4 = AddressBookEntry.Create("richard", "hammond", "007 Long Drive", null, "Salt Lake City",
+			var addressBookEntry4 = AddressBookEntry.Create("richard", "hammond", "007 Long Drive", "", "Salt Lake City",
 				"UT", "84004", "800-456-7890", "753-951-1478", "rh@grandTour.com");
 
 			return new List<AddressBookEntry> {addressBookEntry1, addressBookEntry2, addressBookEntry3, addressBookEntry4};
